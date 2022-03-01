752e15510ea08638353656d7c0c7b434931b455e
Ethan Prieur <ethan.prieur@mths.ca>
2022-03-01 19:24:29 +0000

    Added Area and Perimeter File

---
+++ area_and_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by Ethan Prieur
+# Created on February 2022
+# This is a file that lets you enter Length and Width to find the area and perimeter of a rectangle
+
+import math
+
+
+def main():
+    # The Function Calculates Area and Perimeter
+
+    length_of_rectangle = int(input("Enter The Desired Length (mm): "))
+    width_of_rectangle = int(input("Enter The Desired Width (mm): "))
+
+    area = length_of_rectangle * width_of_rectangle
+    perimeter = 2 * (length_of_rectangle + width_of_rectangle)
+
+    print("")
+    print("Area is {0} mm².".format(area))
+    print("Perimeter is {0} mm.".format(perimeter))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
